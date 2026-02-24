7ef2b2472c269d9211a232f275ef4ed3963159e4
Hanse Kim <hkim2389@wisc.edu>
2026-02-24 16:24:26 -0600

    Cutoff update (misc)

--- num_moduli_cutoff.py
+++ num_moduli_cutoff.py
@@ -41,35 +41,64 @@ def instanton_ev(moduli, gv_d):
     return final
 
 # 
-def rays_multiple(rays, vector):
+def rays_multiple(rays, vector, rtol = 1e-8, atol = 1e-12):
     for ray in rays:
         if np.std(ray/vector) == 0:
             return True 
     return False
 
 # given a sample of moduli, evalute the scaling where the instanton correction upto specified degree is less than specified cutoff
-def cutoff_ev(moduli, p, min_points = int(2e1), cutoff = 1, tol = 1e-2, max_trials = 100):
+def cutoff_ev(moduli, p, min_points = int(2e1), cutoff = 1, tol = 1e-3, max_trials = 100):
     cy = p.triangulate().get_cy()
     rays = cy.toric_kahler_cone().extremal_rays()
 
     coprime_ints = coprime_integer_scaling(moduli)
     scaled_moduli = np.zeros_like(moduli)
+
+    qv_gv_cache = {}
     
     for i in tqdm(range(coprime_ints.shape[0])):
-        #print(cy.toric_kahler_cone().contains(coprime_ints[i]))
         if rays_multiple(rays, coprime_ints[i]):
+            continue 
+        if not cy.toric_kahler_cone().contains(coprime_ints[i]):
             continue
-        print(coprime_ints[i])
+
+        #gvs = cy.compute_gvs(min_points = min_points)
         gvs = cy.compute_gvs(min_points = min_points, grading_vec = coprime_ints[i])
-        #l_i = 1/(2*np.pi) * np.log(sum(gvs.dok.values()))
-        l_i = 1
-        l_curr, iev = l_i, instanton_ev(moduli[i] * l_i, gvs)
-        trials = l_i
-        while np.abs(iev - cutoff) > tol and trials < max_trials:
-            iev = instanton_ev(moduli[i] * l_curr, gvs)
-            l_del = np.clip((iev-cutoff),-(1.1)**(trials-1), (1.1)**(trials-1)) * l_curr * (1.1)**(-trials)
-            l_curr += l_del
-            trials += 1
+
+        def inst(l):
+            return instanton_ev(moduli[i] * l, gvs)
+
+        l_lo, l_hi = 1.0, 2.0
+        iev_lo, iev_hi = inst(l_lo), inst(l_hi) # iev_lo > iev_hi !
+
+        for _ in range(max_trials):
+            if iev_hi < cutoff + tol:
+                break
+            else:
+                l_hi *= 2.0
+                iev_hi = inst(l_hi)
+        for _ in range(max_trials):
+            if iev_lo > cutoff + tol:
+                break
+            else:
+                l_lo /= 2.0
+                iev_lo = inst(l_lo)
+        
+        for _ in range(max_trials):
+            l_mid = 0.5 * (l_lo + l_hi)
+            iev_mid = inst(l_mid)
+
+            if np.abs(iev_mid - cutoff) <= tol:
+                l_lo = l_hi = l_mid
+                break
+
+            if iev_mid > cutoff:
+                l_lo = l_mid
+            else:
+                l_hi = l_mid
+
+            l_curr = l_hi
         
         scaled_moduli[i] = moduli[i] * l_curr
         
@@ -93,7 +122,7 @@ if __name__ == "__main__":
     cutoff = 1
     total_moduli = int(1e3)
     
-    for i in range(3,len(h_s_polytope)):
+    for i in range(len(h_s_polytope)):
         p = h_s_polytope[i]
         rays = p.triangulate().get_cy().toric_kahler_cone().extremal_rays()
         
@@ -101,10 +130,11 @@ if __name__ == "__main__":
                                moduli_sample_factor = int(1), moduli_batch_no = total_moduli)
 
         moduli = cy_obj._moduli_projection_sample()
+        n1_moduli = moduli / np.linalg.norm(moduli, axis = 1).reshape(-1,1)
 
-        scaled_moduli = cutoff_ev(moduli, p, cutoff = 1)
+        scaled_moduli = cutoff_ev(n1_moduli, p, cutoff = 1)
         
-        with open(f"data/num_moduli_cutoff/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}.json", "wb") as f:
+        with open(f"data/num_moduli_cutoff/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}_1.json", "wb") as f:
             pickle.dump(scaled_moduli, f)
 
         plt.close()
@@ -114,4 +144,4 @@ if __name__ == "__main__":
         plt.plot([0,5*rays[0,0]], [0,5*rays[0,1]], color = "red")
         plt.plot([0,5*rays[1,0]], [0,5*rays[1,1]], color = "red")
 
-        plt.savefig(f"figures/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}")
+        plt.savefig(f"figures/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}_1")
