d3b7953331c864e7eaa8bd47b65e19b76c0c78c0
Hanse Kim <hkim2389@wisc.edu>
2026-02-28 17:38:02 -0600

    Improved cutoff calculation

--- num_moduli_cutoff.py
+++ num_moduli_cutoff.py
@@ -29,18 +29,12 @@ def coprime_integer_scaling(moduli, tol = 1e-2, denom_max = 100):
     
     coprime_int[~updated] = np.round(moduli[~updated] * denom_max)
     nonzero = ~np.any(coprime_int == 0, axis = 1)
+    
+    assoc_moduli = moduli[nonzero, :]
     coprime_int = coprime_int[nonzero, :].astype(int)
 
-    return coprime_int
+    return assoc_moduli, coprime_int
 
-# given a dict of gv coeffs, and a moduli point compute instanton correction
-def instanton_ev(moduli, gv_d):
-    final = 0
-    for qv, gv in gv_d.dok.items():
-        final += gv * np.exp(-2*np.pi*(qv @ moduli))
-    return final
-
-# 
 def rays_multiple(rays, vector, rtol = 1e-8, atol = 1e-12):
     for ray in rays:
         if np.std(ray/vector) == 0:
@@ -48,61 +42,92 @@ def rays_multiple(rays, vector, rtol = 1e-8, atol = 1e-12):
     return False
 
 # given a sample of moduli, evalute the scaling where the instanton correction upto specified degree is less than specified cutoff
-def cutoff_ev(moduli, p, min_points = int(2e1), cutoff = 1, tol = 1e-3, max_trials = 100):
+    # if parallel, gv invariants are stored in memory
+def cutoff_ev_qvs_moduli(gvs, qvs, moduli, cutoff, max_trials, tol):
+    N = moduli.shape[0]
+    exponent = -2*np.pi*np.einsum("Nmd,Nd->Nm", qvs, moduli)
+
+    def log_inst(l):
+        weighted_exponent = l[:,None] * exponent
+        return np.log(np.einsum("Nm,Nm->N", gvs, np.exp(weighted_exponent)))
+
+    l_lo, l_hi = 1.0 * np.ones((N,)), 2.0 * np.ones((N,))
+    iev_lo, iev_hi = log_inst(l_lo), log_inst(l_hi) # iev_lo > iev_hi !
+    
+    for _ in range(max_trials):
+        l_hi_bool = (iev_hi < np.log(cutoff))
+        if (~l_hi_bool).all():
+            break
+        else:
+            l_hi[l_hi_bool] /= 2.0
+            iev_hi = log_inst(l_hi)
+    for _ in range(max_trials):
+        l_lo_bool = (iev_lo > np.log(cutoff))
+        if (~l_lo_bool).all():
+            break
+        else:
+            l_lo[l_lo_bool] *= 2.0
+            iev_lo = log_inst(l_lo)
+    
+    #print(f"lo : {moduli[i] * l_lo}, hi : {moduli[i] * l_hi}")
+    for _ in range(max_trials):
+        l_mid = 0.5 * (l_lo + l_hi)
+        iev_mid = log_inst(l_mid)
+        
+        l_mid_bool = (np.abs(iev_mid - np.log(cutoff)) <= tol / iev_mid)
+        l_mid_hi_bool = (iev_mid - np.log(cutoff) > tol / iev_mid)
+        l_mid_lo_bool = (iev_mid - np.log(cutoff) < -tol/ iev_mid)
+
+        if l_mid_bool.all():
+            break
+        else:
+            l_lo[l_mid_bool] = l_hi[l_mid_bool] = l_mid[l_mid_bool]
+            l_hi[l_mid_hi_bool] = l_mid[l_mid_hi_bool]
+            l_lo[l_mid_lo_bool] = l_mid[l_mid_lo_bool]
+        
+    scaled_moduli = moduli * l_mid[:,None]
+
+    return scaled_moduli
+
+def cutoff_ev(moduli, p, min_points = int(2e1), cutoff = 1, tol = 1e-3, max_trials = 100,
+                   precise_grading = True, parallel = True): 
+
     cy = p.triangulate().get_cy()
     rays = cy.toric_kahler_cone().extremal_rays()
 
-    coprime_ints = coprime_integer_scaling(moduli)
+    # update moduli; exclude points where one of rounded integers is 0
+    moduli, coprime_ints = coprime_integer_scaling(moduli)
     scaled_moduli = np.zeros_like(moduli)
 
-    qv_gv_cache = {}
+    N, d = moduli.shape[0], moduli.shape[1]
+
+    qvs = np.zeros((N, min_points, d))
+    gvs = np.zeros((N, min_points))
+
+    gv_dict_default = cy.compute_gvs(min_points = round(min_points*1.2))
     
-    for i in tqdm(range(coprime_ints.shape[0])):
-        if rays_multiple(rays, coprime_ints[i]):
-            continue 
-        if not cy.toric_kahler_cone().contains(coprime_ints[i]):
+    for i in tqdm(range(N)):
+        if rays_multiple(rays, coprime_ints[i]) or not cy.toric_kahler_cone().contains(coprime_ints[i]):
+            # update moduli; exclude points where gv invariant is not defined
             continue
-
-        #gvs = cy.compute_gvs(min_points = min_points)
-        gvs = cy.compute_gvs(min_points = min_points, grading_vec = coprime_ints[i])
-
-        def inst(l):
-            return instanton_ev(moduli[i] * l, gvs)
-
-        l_lo, l_hi = 1.0, 2.0
-        iev_lo, iev_hi = inst(l_lo), inst(l_hi) # iev_lo > iev_hi !
-
-        for _ in range(max_trials):
-            if iev_hi < cutoff + tol:
-                break
-            else:
-                l_hi *= 2.0
-                iev_hi = inst(l_hi)
-        for _ in range(max_trials):
-            if iev_lo > cutoff + tol:
-                break
-            else:
-                l_lo /= 2.0
-                iev_lo = inst(l_lo)
         
-        #print(f"lo : {moduli[i] * l_lo}, hi : {moduli[i] * l_hi}")
-        for _ in range(max_trials):
-            l_mid = 0.5 * (l_lo + l_hi)
-            iev_mid = inst(l_mid)
+        if precise_grading:
+            # this step is the most time consuming
+            gv_dict = cy.compute_gvs(min_points = round(min_points*1.2), grading_vec = coprime_ints[i]).dok
+        else:
+            gv_dict = gv_dict_default
 
-            if np.abs(iev_mid - cutoff) <= tol:
-                l_lo = l_hi = l_mid
-                break
-
-            if iev_mid > cutoff:
-                l_lo = l_mid
-            else:
-                l_hi = l_mid
-
-            l_curr = l_hi
-            #print(l_curr * moduli[i])
+        gvs_i, qvs_i = np.array(list(gv_dict.values())[:min_points]), np.array(list(gv_dict.keys())[:min_points])
         
-        scaled_moduli[i] = moduli[i] * l_curr
+        if parallel:
+            gvs[i] = gvs_i
+            qvs[i] = qvs_i
+        else: # compute using gv invariants then discard
+            gvs_i, qvs_i, moduli_i = gvs_i.reshape(1, min_points), qvs_i.reshape(1, min_points, d), moduli[i].reshape(1, d)
+            scaled_moduli[i] = cutoff_ev_qvs_moduli(gvs_i, qvs_i, moduli_i, cutoff, max_trials, tol)
+
+    if parallel:
+        scaled_moduli = cutoff_ev_qvs_moduli(gvs, qvs, moduli, cutoff, max_trials, tol)
         
     return scaled_moduli
 
@@ -122,10 +147,10 @@ if __name__ == "__main__":
     #rho = index_density(h_s)
     moduli_max = 5
     cutoff = 1
-    total_moduli = int(1e3)
+    total_moduli = int(1e1*10**h_s)
     
-    #for i in range(len(h_s_polytope)):
-    for i in range(13,14):
+    # for i in range(len(h_s_polytope)):
+    for i in range(0,1):
         p = h_s_polytope[i]
         rays = p.triangulate().get_cy().toric_kahler_cone().extremal_rays()
         
@@ -134,21 +159,22 @@ if __name__ == "__main__":
 
         cy = p.triangulate().get_cy()
         gvs = cy.compute_gvs(min_points = 400)
-        print(gvs.dok.items())
         
-        # moduli = cy_obj._moduli_projection_sample()
-        # n1_moduli = moduli / np.linalg.norm(moduli, axis = 1).reshape(-1,1)
-        #
-        # scaled_moduli = cutoff_ev(n1_moduli, p, cutoff = 1, max_trials=300)
-        # 
-        # with open(f"data/num_moduli_cutoff/2_num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}.json", "wb") as f:
-        #     pickle.dump(scaled_moduli, f)
-        #
-        # plt.close()
-        # plt.scatter(scaled_moduli[:,0], scaled_moduli[:,1], s = 1)
-        # plt.xlim((-5, 5))
-        # plt.ylim((-5, 5))
-        # plt.plot([0,5*rays[0,0]], [0,5*rays[0,1]], color = "red")
-        # plt.plot([0,5*rays[1,0]], [0,5*rays[1,1]], color = "red")
-        #
-        # plt.savefig(f"figures/2_num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}")
+        moduli = cy_obj._moduli_projection_sample()
+        n1_moduli = moduli / np.linalg.norm(moduli, axis = 1).reshape(-1,1)
+
+        scaled_moduli = cutoff_ev(n1_moduli, p, cutoff = 1, max_trials=300,
+                                  precise_grading = True, parallel = False)
+
+        with open(f"data/num_moduli_cutoff/3_num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}.json", "wb") as f:
+            pickle.dump(scaled_moduli, f)
+
+        plt.close()
+        plt.scatter(n1_moduli[:,0], n1_moduli[:,1], s = 1, color = "green")
+        plt.scatter(scaled_moduli[:,0], scaled_moduli[:,1], s = 1)
+        plt.xlim((-5, 5))
+        plt.ylim((-5, 5))
+        plt.plot([0,5*rays[0,0]], [0,5*rays[0,1]], color = "red")
+        plt.plot([0,5*rays[1,0]], [0,5*rays[1,1]], color = "red")
+
+        plt.savefig(f"figures/3_num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}")
--- num_moduli_cutoff_test.py
+++ num_moduli_cutoff_test.py
@@ -24,24 +24,13 @@ moduli_max = 5
 cutoff = 1
 total_moduli = int(1e3)
 
-i = 0
-
 h_s = 2
 #p = Polytope([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[-1,-1,-6,-9]])
 h_s_polytope = fetch_polytopes(h11 = h_s, lattice = "N", limit = 100)
 
-p = h_s_polytope[i]
-rays = p.triangulate().get_cy().toric_kahler_cone().extremal_rays()
-cy = p.triangulate().get_cy()
-gvs = cy.compute_gvs(min_points = int(2e3))
-
 #|%%--%%| <qFNo0QmB0U|7qJrRiUIyq>
 
-with open(f"data/num_moduli_cutoff/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}_1.json", "rb") as f:
-    moduli = pickle.load(f)
-
-def cutoff_check(moduli_sp):
-    gvs_og = gvs.dok
+def cutoff_check(moduli_sp, gvs_og):
     gvs_trunc = dict(list(gvs.dok.items())[:int(1e2)])
 
     gvs_subset = multiples_subset(gvs_og, gvs_trunc, kmax=5)
@@ -62,14 +51,25 @@ def cutoff_check(moduli_sp):
 
     return exp_curves
 
-#|%%--%%| <7qJrRiUIyq|A6AT9AEISj>
+#|%%--%%| <7qJrRiUIyq|N2xXeU5zRI>
+
+# check the exponential convergence of individual instanton corrections for points suggested to have converging instanton corrections
+
+i = 0
+with open(f"data/num_moduli_cutoff/num_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}_1.json", "rb") as f:
+    moduli = pickle.load(f)
 
 plt.close('all')
+
+p = h_s_polytope[i]
+rays = p.triangulate().get_cy().toric_kahler_cone().extremal_rays()
+cy = p.triangulate().get_cy()
+gvs = cy.compute_gvs(min_points = int(2e3))
 
 moduli_sp = np.array([[0.4,0.4],[0.5,0.5],[0.6,0.6],[0.7,0.7],[0.8,0.8],[0.9,0.9]])
 
 for tar in range(moduli_sp.shape[0]):
-    exp_curves = cutoff_check(moduli_sp[tar])
+    exp_curves = cutoff_check(moduli_sp[tar], gvs.dok)
     plt.close()
     fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(6, 9), constrained_layout=True)
 
@@ -81,5 +81,6 @@ for tar in range(moduli_sp.shape[0]):
     ax0.plot([0, 5*rays[1, 0]], [0, 5*rays[1, 1]], color="red")
 
     ax1.plot(exp_curves[0].T)
-
+    
     plt.savefig(f"figures/check_moduli_cutoff={cutoff}_mm={moduli_max}_tm={total_moduli*10}_hs={h_s}_ind={i}_tar={tar}")
+
